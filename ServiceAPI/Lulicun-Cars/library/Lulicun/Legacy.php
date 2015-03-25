ec59f9958bb66b69f407c5b1078c47616ae54085
cz-Michael <zchning@gmail.com>
2015-03-24 23:32:09 -0400

    enable Lulicun lagacy to count ids

--- ServiceAPI/Lulicun-Cars/library/Lulicun/Legacy.php
+++ ServiceAPI/Lulicun-Cars/library/Lulicun/Legacy.php
@@ -1,19 +1,29 @@
 <?php
 
 /**
-*TODO: This class is used to do statistics of Lulicun.
+ *This class is used to do statistics of Lulicun.
 **/
 Class Lulicun_Legacy extends Lulicun_Dbo{
 
+	protected static $_collection = 'counter';
+
+	private static function _create($counter_id) {
+		$counter = array('_id' => $counter_id, 'c' => 0);
+		parent::create($counter);
+	}
+
 	public static function incrementCounter($counter_id) {
 		$db = self::getDb();
-		$value = $db->counters->findAndModify(
+		$isInitialized = $db->{static::$_collection}->findOne(array('_id' => $counter_id), array('c' => 1));
+		if (!$isInitialized) {
+			self::_create($counter_id);
+		}
+		$value = $db->{static::$_collection}->findAndModify(
             array('_id' => $counter_id), // update this counter
             array('$inc' => array('c' => 1)), // increment c by one
             array('c' => 1), // return field 'c' with the modified document
             array('new' => true) // return the modified document
         );
-        error_log("legacy: " . json_encode($value));
         // return the "auto-incremented" id
         return $value['c'];
 	}
