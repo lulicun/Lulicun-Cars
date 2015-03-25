c384ba068d85624964367ae00db69ff1109e34be
cz-Michael <zchning@gmail.com>
2015-03-24 23:32:57 -0400

    add base controller to initialize basic settings for application

---
+++ ServiceAPI/Lulicun-Cars/application/controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *	TODO:
+ *		Initialize session ...
+ **/
+
+class BaseController extends Zend_Controller_Action {
+
+	public function init() {
+		parent::init();
+		//TODO: ... Initialize
+	}
+
+}
